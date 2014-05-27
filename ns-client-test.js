00ab3b7676cc88f001b34dcfc9744e6c168820ea
Thisara Rupasinghe <thisarattr@gmail.com>
2014-05-27 22:49:47 +0800

    client test

--- ns-client-test.js
+++ ns-client-test.js
@@ -1,7 +1,8 @@
 var nsClient = require('ns-nodejs-client');
 
 var notification = '{"notificationType":3,"smsData":{"subject":"Test","content":"This is test msg from REDMART","recipients":["6592329052"]},"ack":true,"errorUrl":""}';
+var env = nsClient.env("local");
 
-nsClient.sendNotification(nsClient.LOCAL, notification);
+nsClient.sendNotification(env, notification);
 
 console.log("notification success!!!");
